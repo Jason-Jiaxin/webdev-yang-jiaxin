dbdbbba76bceb81f551a68dffe05a6c5a4c70fc1
Jiaxin Yang <jiaxinn.yang@gmail.com>
2017-10-10 13:52:06 -0400

    Services added

---
+++ src/app/services/website.service.client.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+// injecting service into module
+@Injectable()
+export class WebsiteService {
+
+  constructor() { }
+
+  websites = [
+    { '_id': '123', 'name': 'Facebook',    'developerId': '456', 'description': 'Lorem' },
+    { '_id': '234', 'name': 'Tweeter',     'developerId': '456', 'description': 'Lorem' },
+    { '_id': '456', 'name': 'Gizmodo',     'developerId': '456', 'description': 'Lorem' },
+    { '_id': '890', 'name': 'Go',          'developerId': '123', 'description': 'Lorem' },
+    { '_id': '567', 'name': 'Tic Tac Toe', 'developerId': '123', 'description': 'Lorem' },
+    { '_id': '678', 'name': 'Checkers',    'developerId': '123', 'description': 'Lorem' },
+    { '_id': '789', 'name': 'Chess',       'developerId': '234', 'description': 'Lorem' }
+  ];
+
+  api = {
+    'createWebsite'   : this.createWebsite,
+    'findWebsitesByUser' : this.findWebsitesByUser,
+    'findWebsiteById' : this.findWebsiteById,
+    'updateWebsite' : this.updateWebsite,
+    'deleteWebsite' : this.deleteWebsite
+  };
+
+  createWebsite(userId, website) {
+    website._id = Math.random();
+    website.developerId = userId;
+    this.websites.push(website);
+    return(website);
+  }
+
+  findWebsitesByUser(userId) {
+    const res = [];
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x].developerId === userId) {
+        res.push(this.websites[x]);
+      }
+    }
+    return res;
+  }
+
+  findWebsiteById(websiteId) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        return this.websites[x];
+      }
+    }
+  }
+
+  updateWebsite(websiteId, website) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        this.websites[x] = website;
+      }
+    }
+  }
+
+  deleteWebsite(websiteId) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        this.websites.splice(x, 1);
+      }
+    }
+  }
+}
