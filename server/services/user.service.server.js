27dd891735be49dad9ee2da2e306cff6b7533e92
Jiaxin Yang <jiaxinn.yang@gmail.com>
2017-11-18 21:03:50 -0500

    encrypt password

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
 
   let userModel = require('../model/user/user.model.server');
+  let bcrypt = require("bcrypt-nodejs");
 
   let users = [
     {_id: '123', username: 'alice', password: 'alice', firstName: 'Alice', lastName: 'Wonder'},
@@ -79,11 +80,11 @@ module.exports = function (app) {
   function localStrategy(username, password, done) {
     console.log('passport local strategy');
     userModel
-      .findUserByCredentials(username, password)
+      .findUserByUsername(username)
       .then(
         function(user) {
           console.log(user);
-          if(user && user.username === username && user.password === password) {
+          if(user && user.username === username && bcrypt.compareSync(password, user.password)) {
             return done(null, user);
           } else {
             console.log('passport else no user');
@@ -99,6 +100,7 @@ module.exports = function (app) {
 
   function register(req, res) {
     let user = req.body;
+    user.password = bcrypt.hashSync(user.password);
     userModel
       .createUser(user)
       .then(function(user){
